48152d2d92a4cc52f6857eff5d3941964a22b9f0
sharon oloo <oloosharon@gmail.com>
2022-10-15 00:25:59 +0300

    pick date values from html form to JS

--- js/custom.js
+++ js/custom.js
@@ -3,11 +3,15 @@ const FEMALE_NAMES=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 const DAYS_OF_THE_WEEK=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 function getUserInputAndCalculateDay()
 {
-    var year=(prompt("Enter year: "));
+    var year=document.getElementById("year-input").value;
+    console.log(year);
     var century=parseInt(year.substring(0,2));
-    year = parseInt(year);
-    var month = parseInt(prompt("Enter month: "));
-    var day=parseInt(prompt("Enter day: "));
+    console.log(century);
+    //year = parseInt(year);
+    var month = document.getElementById("month-input").value;
+    console.log(month);
+    var day=document.getElementById("day-input").value;
+    console.log(day);
     var dayOfTheWeek = Math.round( (( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
 
     return dayOfTheWeek;
