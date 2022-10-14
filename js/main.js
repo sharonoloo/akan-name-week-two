e711ba5ab04ae908a222c2d341eaffcb3acb2569
sharon oloo <oloosharon@gmail.com>
2022-10-14 16:31:33 +0300

    update business logic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const MALE_NAMES=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const FEMALE_NAMES=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+const DAYS_OF_THE_WEEK=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 function getUserInputAndCalculateDay()
 {
     var year=(prompt("Enter year: "));
@@ -5,12 +8,27 @@ function getUserInputAndCalculateDay()
     year = parseInt(year);
     var month = parseInt(prompt("Enter month: "));
     var day=parseInt(prompt("Enter day: "));
-    var dayOfTheWeek = Math.round( (( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
+    var dayOfTheWeek = Math.round( (( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
 
     return dayOfTheWeek;
 }
 
-console.log(getUserInputAndCalculateDay());
+
+function getGender(){
+    var gender = prompt("Enter your gender:");
+    return gender;
+}
+function main(){
+    var day = getUserInputAndCalculateDay();
+    var gender = getGender();
+    
+    if (gender.toLowerCase().startsWith("f")){
+        akanName = FEMALE_NAMES[day];
+    }else{
+        akanName = MALE_NAMES[day];
+    }
+    alert("Your Akan Name is "+akanName);
+}
 
 
 
